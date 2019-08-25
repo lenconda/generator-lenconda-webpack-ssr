bfd032e482a47d222125359c91f033e5bb9f2abe
lenconda <i@lenconda.top>
2019-08-26 01:05:46 +0800

    :sparkles: Add common code template

---
+++ generators/app/templates/common/src/pages/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.<%= extension %>';
+
+const App = () => {
+  return (
+    <p className="it-works">
+      It works!
+    </p>
+  );
+};
+
+export default App;
---
+++ generators/app/templates/common/src/pages/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.<%= extension %>';
+
+const App = (): JSX.Element => {
+  return (
+    <p className="it-works">
+      It works!
+    </p>
+  );
+};
+
+export default App;
