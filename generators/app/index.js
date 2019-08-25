16850e474a650e463b2d280bfa01385b7d20eb5f
lenconda <i@lenconda.top>
2019-08-26 01:06:09 +0800

    :see_no_evil: Add gitignore file for template

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,9 +5,9 @@ const yosay = require('yosay');
 const mkdirp = require('mkdirp');
 const path = require('path');
 
-const PREPROCESS_SASS = 'sass_scss';
+const PREPROCESS_SASS = 'scss';
 const PREPROCESS_LESS = 'less';
-const PREPROCESS_STYLUS = 'stylus';
+const PREPROCESS_STYLUS = 'styl';
 const PREPROCESS_ORIGINAL = 'original';
 
 const DEPENDENCIES_SASS = {
@@ -29,10 +29,10 @@ const DEPENDENCIES_STYLUS = {
   }
 };
 const styleDependencies = {
-  'sass_scss': DEPENDENCIES_SASS,
+  scss: DEPENDENCIES_SASS,
   less: DEPENDENCIES_LESS,
-  stylus: DEPENDENCIES_STYLUS
-} 
+  styl: DEPENDENCIES_STYLUS
+};
 
 module.exports = class extends Generator {
   prompting() {
@@ -111,9 +111,50 @@ module.exports = class extends Generator {
       }
     );
 
-
     if (this.props.preprocessor !== 'original')
       this.fs.extendJSON(this.destinationPath('package.json'), styleDependencies[this.props.preprocessor]);
+
+    this.fs.copy(this.templatePath('common/scripts'), this.destinationPath('scripts'));
+    this.fs.copy(this.templatePath('common/_.gitignore'), this.destinationPath('.gitignore'));
+    this.fs.copy(this.templatePath('common/postcss.config.js'), this.destinationPath('postcss.config.js'));
+
+    if (this.props.typescript) {
+      this.fs.copy(this.templatePath('typescript/_tsconfig.json'), this.destinationPath('tsconfig.json'));
+      this.fs.copy(this.templatePath('typescript/_.babelrc'), this.destinationPath('.babelrc'));
+      this.fs.copy(this.templatePath('typescript/_.eslintrc.js'), this.destinationPath('.eslintrc.js'));
+      this.fs.copy(this.templatePath('typescript/src'), this.destinationPath('src'));
+      this.fs.copy(this.templatePath('typescript/server'), this.destinationPath('server'));
+      this.fs.copy(this.templatePath('typescript/typings'), this.destinationPath('typings'));
+    } else {
+      this.fs.copy(this.templatePath('javascript/src'), this.destinationPath('src'));
+      this.fs.copy(this.templatePath('javascript/server'), this.destinationPath('server'));
+      this.fs.copy(this.templatePath('javascript/_.babelrc'), this.destinationPath('.babelrc'));
+      this.fs.copy(this.templatePath('javascript/_.eslintrc.js'), this.destinationPath('.eslintrc.js'));
+    }
+
+    this.fs.copy(this.templatePath('common/src/config/_config.js'), this.destinationPath('src/config/config.js'));
+      
+    if (this.props.preprocessor !== 'original') {
+      this.fs.copyTpl(
+        this.templatePath('common/src/pages/App.' + (this.props.typescript ? 'tsx' : 'jsx')),
+        this.destinationPath('src/pages/App.' + (this.props.typescript ? 'tsx' : 'jsx')),
+        {
+          extension: this.props.preprocessor
+        }
+      );
+      this.fs.copy(this.templatePath('common/src/pages/App.' + this.props.preprocessor), this.destinationPath('src/pages/App.' + this.props.preprocessor));
+      this.fs.copy(this.templatePath('common/src/config/_' + this.props.preprocessor + '_loader.js'), this.destinationPath('src/config/css_loaders.js'));
+    } else {
+      this.fs.copyTpl(
+        this.templatePath('common/src/pages/App.' + (this.props.typescript ? 'tsx' : 'jsx')),
+        this.destinationPath('src/pages/App.' + (this.props.typescript ? 'tsx' : 'jsx')),
+        {
+          extension: 'css'
+        }
+      );
+      this.fs.copy(this.templatePath('common/src/pages/App.css'), this.destinationPath('src/pages/App.css'));
+      this.fs.copy(this.templatePath('common/src/config/_css_loader.js'), this.destinationPath('src/config/css_loaders.js'));
+    }
   }
 
   install() {
