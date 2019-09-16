8d9751b0ac64da1b5524d37ddc49984ab428e841
lenconda <i@lenconda.top>
2019-09-16 21:23:10 +0800

    :art: Add Alloy eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
+  extends: ['eslint-config-alloy'],
   globals: {},
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   rules: {
     indent: [
       'error',
       2
     ],
+    'semi': 'error',
     'eol-last': 2,
     quotes: [2, 'single']
   }
